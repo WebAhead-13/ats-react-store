8844a56358bbd4ea83dde8fbb6543d186ac62eaa
thaier bashir <tba2095@gmail.com>
2022-02-12 18:15:21 +0200

    dataUpdatet needs css

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -18,6 +18,48 @@ const data = {
       price: 1000,
       image: "https://picsum.photos/id/21/3008/2008",
     },
+    {
+      id: "4",
+      name: "Headphones",
+      price: 150,
+      image:
+        "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: "5",
+      name: "camera",
+      price: 435,
+      image:
+        "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      id: "6",
+      name: "bottle",
+      price: 531,
+      image:
+        "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZHVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      id: "7",
+      name: "sneakers",
+      price: 453,
+      image:
+        "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      id: "8",
+      name: "bike",
+      price: 353,
+      image:
+        "https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      id: "9",
+      name: "W xbox",
+      price: 1000,
+      image:
+        "https://images.unsplash.com/photo-1600080972464-8e5f35f63d08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60",
+    },
   ],
 };
 export default data;
